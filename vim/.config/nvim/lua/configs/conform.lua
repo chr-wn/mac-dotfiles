927dd27716f86d6cba5cbfd858fec68f7abda4e5
Charlie Wang <charliewang109105116@gmail.com>
2025-08-26 03:00:01 -0400

    repo backup: 2025-08-26 03:00:01

--- vim/.config/nvim/lua/configs/conform.lua
+++ vim/.config/nvim/lua/configs/conform.lua
@@ -12,17 +12,17 @@ local options = {
   format_on_save = false,
   
   formatters = {
-    black = {
-      prepend_args = { "--fast", "--line-length=88" },
-    },
-    ["clang-format"] = {
-      prepend_args = { 
-        "--style={BasedOnStyle: Google, IndentWidth: 2, TabWidth: 2, UseTab: Never}" 
-      },
-    },
-    stylua = {
-      prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
-    },
+    -- black = {
+    --   prepend_args = { "--fast", "--line-length=88" },
+    -- },
+    -- ["clang-format"] = {
+    --   prepend_args = { 
+    --     "--style={BasedOnStyle: Google, IndentWidth: 2, TabWidth: 2, UseTab: Never}" 
+    --   },
+    -- },
+    -- stylua = {
+    --   prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
+    -- },
   },
 }
 
--- vim/.config/nvim/lua/options.lua
+++ vim/.config/nvim/lua/options.lua
@@ -1,31 +1,30 @@
 require "nvchad.options"
 
--- add yours here!
 vim.wo.relativenumber = true
 vim.wo.number = true
 
 vim.opt.scrolloff = 3
 
--- Set 2-space indentation
-vim.opt.tabstop = 2        -- Number of spaces that a <Tab> in the file counts for
-vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
-vim.opt.softtabstop = 2    -- Number of spaces that a <Tab> counts for while editing
-vim.opt.expandtab = true   -- Use spaces instead of tabs
+-- -- Set 2-space indentation
+-- vim.opt.tabstop = 2        -- Number of spaces that a <Tab> in the file counts for
+-- vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
+-- vim.opt.softtabstop = 2    -- Number of spaces that a <Tab> counts for while editing
+-- vim.opt.expandtab = true   -- Use spaces instead of tabs
 
--- Force 2-space indentation for all filetypes (including Python)
-vim.api.nvim_create_augroup('ForceIndentation', { clear = true })
-vim.api.nvim_create_autocmd('FileType', {
-  group = 'ForceIndentation',
-  pattern = '*',
-  callback = function()
-    vim.bo.tabstop = 2
-    vim.bo.shiftwidth = 2
-    vim.bo.softtabstop = 2
-    vim.bo.expandtab = true
-  end,
-})
+-- -- Force 2-space indentation for all filetypes (including Python)
+-- vim.api.nvim_create_augroup('ForceIndentation', { clear = true })
+-- vim.api.nvim_create_autocmd('FileType', {
+--   group = 'ForceIndentation',
+--   pattern = '*',
+--   callback = function()
+--     vim.bo.tabstop = 2
+--     vim.bo.shiftwidth = 2
+--     vim.bo.softtabstop = 2
+--     vim.bo.expandtab = true
+--   end,
+-- })
 
--- Highlight on yank (native Neovim feature using Lua)
+-- Highlight on yank (native nvim)
 vim.api.nvim_create_augroup('YankHighlight', { clear = true })
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = 'YankHighlight',
